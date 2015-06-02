42a53df27e61e5b90017c855a3f7085e54bf3a53
eyolas <dtouzet@gmail.com>
2015-06-02 10:34:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+superagent-es6-promise
+===========================
+
+
+Add promise support to
+[Superagent](http://visionmedia.github.io/superagent/).
+
+it was initially forked from [superagent-bluebird-promise](https://github.com/KyleAMathews/superagent-bluebird-promise).
+
+## Install
+`npm install superagent-es6-promise`
+
+## Usage
+Simply require this package instead of `superagent`. Then you can call `.then()` instead of `.end()` to get a promise for your requests.
+
+```javascript
+var request = require('superagent-bluebird-promise');
+
+request.get('/an-endpoint')
+  .then(function(res) {
+    console.log(res);
+  }, function(error) {
+    console.log(error);
+  });
+```
+
+To generate a promise without registering any callbacks (e.g. when returning a promise from within a library), call `.promise()` instead.
+
+```javascript
+request.get('/an-endpoint').promise()
+```
+
+
+An error is thrown for all HTTP errors and responses that have a response code of 400 or above.
+
+The `error` parameter always has a key `error` and for 4xx and 5xx responses, will also have a `status` and `res` key.
---
+++ index.js
@@ -0,0 +1,65 @@
+// So you can `var request = require("superagent-es6-promise")`
+var superagent = module.exports = require("superagent");
+var Request = superagent.Request;
+
+// Create custom error type.
+// Create a new object, that prototypally inherits from the Error constructor.
+var SuperagentPromiseError = superagent.SuperagentPromiseError = function (message) {
+  this.name = 'SuperagentPromiseError';
+  this.message = message || 'Bad request';
+};
+
+SuperagentPromiseError.prototype = new Error();
+SuperagentPromiseError.prototype.constructor = SuperagentPromiseError;
+
+/**
+ * @namespace utils
+ * @class Superagent
+ */
+
+/**
+ *
+ * Add promise support for superagent/supertest
+ *
+ * Call .promise() to return promise for the request
+ *
+ * @method then
+ * @return {Bluebird.Promise}
+ */
+Request.prototype.promise = function() {
+  var req = this;
+  var error;
+
+  return new Promise(function(resolve, reject) {
+      req.end(function(err, res) {
+        if (typeof res !== "undefined" && res.status >= 400) {
+          var msg = 'cannot ' + req.method + ' ' + req.url + ' (' + res.status + ')';
+          error = new SuperagentPromiseError(msg);
+          error.status = res.status;
+          error.body = res.body;
+          error.res = res;
+          reject(error);
+        } else if (err) {
+          reject(new SuperagentPromiseError(err));
+        } else {
+          resolve(res);
+        }
+      });
+    });
+};
+
+/**
+ *
+ * Make superagent requests Promises/A+ conformant
+ *
+ * Call .then([onFulfilled], [onRejected]) to register callbacks
+ *
+ * @method then
+ * @param {function} [onFulfilled]
+ * @param {function} [onRejected]
+ * @return {Bluebird.Promise}
+ */
+Request.prototype.then = function() {
+  var promise = this.promise();
+  return promise.then.apply(promise, arguments);
+};
